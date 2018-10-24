4a8bd8841027496e04a1038b35636d250a2ac9e5
Ari Kardasis <ari.kardasis@gmail.com>
2018-10-24 20:48:44 +0000

    implemented off and toggle:

--- src/index.js
+++ src/index.js
@@ -9,9 +9,32 @@ const led = new Gpio(21, 'out')
 
 led.writeSync(0)
 
-app.get('/', (req, res) => {
-  console.log('message')
+app.get('/heat/on', (req, res) => {
+  console.log('turning heat on')
   led.writeSync(1)
-}) 
+  res.send('the heat is on')
+})
+
+app.get('/heat/off', (req, res) => {
+  console.log('turning heat off')
+  led.writeSync(0)
+  res.send('the heat is off')
+})
+
+app.get('/heat/toggle', (req, res) => {
+  let state = led.readSync()
+  let newState = 1 - state
+  console.log('old state : ' + state + '  ---  new state : ' + newState)
+  console.log('toggling heat')
+  led.writeSync(newState)
+  if (newState) {
+    res.send('the heat is on')
+  } else { 
+    res.send('the heat is off')
+  }
+})
+
 app.listen(port, () => { 
+  console.log('listening on port : ' + port)
+  led.writeSync(0)
 })
